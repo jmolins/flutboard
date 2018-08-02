fc57561ac4b6b0dbd0d202f76962505605dc49d4
jmolins <chemamolins@gmail.com>
2018-08-02 22:46:15 +0200

    Upgrade RxDart and some formatting

--- lib/ui/article_page.dart
+++ lib/ui/article_page.dart
@@ -29,10 +29,7 @@ class ArticlePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    var screenWidth = MediaQuery
-        .of(context)
-        .size
-        .width;
+    var screenWidth = MediaQuery.of(context).size.width;
 
     Icon _getMenuIcon(TargetPlatform platform) {
       assert(platform != null);
@@ -49,25 +46,22 @@ class ArticlePage extends StatelessWidget {
     return Container(
       color: Colors.white,
       height: height,
-      width: MediaQuery
-          .of(context)
-          .size
-          .width,
+      width: MediaQuery.of(context).size.width,
       child: Column(
         children: [
           AppBar(
             leading: flipBack != null
                 ? new IconButton(
-              icon: new Icon(Icons.arrow_back),
-              color: Colors.black87,
-              onPressed: flipBack,
-            )
+                    icon: new Icon(Icons.arrow_back),
+                    color: Colors.black87,
+                    onPressed: flipBack,
+                  )
                 : Padding(
-              padding: const EdgeInsets.all(10.0),
-              child: Image.asset(
-                'assets/images/flutboard_logo.png',
-              ),
-            ),
+                    padding: const EdgeInsets.all(10.0),
+                    child: Image.asset(
+                      'assets/images/flutboard_logo.png',
+                    ),
+                  ),
             title: Text(
               article.source,
               style: TextStyle(color: Colors.black87),
@@ -78,26 +72,25 @@ class ArticlePage extends StatelessWidget {
             actions: <Widget>[
               flipBack == null
                   ? IconButton(
-                icon: new Icon(Icons.refresh),
-                //color: Colors.black87,
-                onPressed: () =>
-                    ArticleBlocProvider
-                        .of(context)
-                        .getArticles(refresh: true),
-              )
+                      icon: new Icon(Icons.refresh),
+                      //color: Colors.black87,
+                      onPressed: () => ArticleBlocProvider
+                          .of(context)
+                          .getArticles(refresh: true),
+                    )
                   : Container(),
               PopupMenuButton<String>(
                 itemBuilder: (BuildContext context) {
                   return <PopupMenuEntry<String>>[
                     flipBack == null
                         ? PopupMenuItem<String>(
-                      value: 'sources',
-                      child: Text('Select Sources'),
-                    )
+                            value: 'sources',
+                            child: Text('Select Sources'),
+                          )
                         : PopupMenuItem<String>(
-                      value: 'back',
-                      child: Text('Back to Top'),
-                    ),
+                            value: 'back',
+                            child: Text('Back to Top'),
+                          ),
                     PopupMenuItem<String>(
                       value: 'about',
                       child: Text('About'),
@@ -118,14 +111,16 @@ class ArticlePage extends StatelessWidget {
           SizedBox(
             width: screenWidth,
             //height: screenWidth / 2,
-            child: FadeInImage.assetNetwork(
-              placeholder: 'assets/images/1x1_transparent.png',
-              image: article.urlToImage,
-              width: screenWidth,
-              height: screenWidth / 2,
-              fadeInDuration: const Duration(milliseconds: 300),
-              fit: BoxFit.cover,
-            ),
+            child: article.urlToImage != null
+                ? FadeInImage.assetNetwork(
+                    placeholder: 'assets/images/1x1_transparent.png',
+                    image: article.urlToImage,
+                    width: screenWidth,
+                    height: screenWidth / 2,
+                    fadeInDuration: const Duration(milliseconds: 300),
+                    fit: BoxFit.cover,
+                  )
+                : Container(),
           ),
           Padding(
             padding: EdgeInsets.all(10.0),
@@ -153,13 +148,13 @@ class ArticlePage extends StatelessWidget {
               padding: EdgeInsets.all(10.0),
               child: new LayoutBuilder(
                   builder: (BuildContext context, BoxConstraints constraints) {
-                    return new Text(
-                      article.description,
-                      overflow: TextOverflow.ellipsis,
-                      style: TextStyle(fontSize: 18.0, color: Colors.black54),
-                      maxLines: (constraints.maxHeight / 18.0).floor() - 1,
-                    );
-                  }),
+                return new Text(
+                  article.description,
+                  overflow: TextOverflow.ellipsis,
+                  style: TextStyle(fontSize: 18.0, color: Colors.black54),
+                  maxLines: (constraints.maxHeight / 18.0).floor() - 1,
+                );
+              }),
             ),
           ),
           Row(
@@ -176,9 +171,7 @@ class ArticlePage extends StatelessWidget {
                 onPressed: null,
               ),
               IconButton(
-                icon: _getMenuIcon(Theme
-                    .of(context)
-                    .platform),
+                icon: _getMenuIcon(Theme.of(context).platform),
                 onPressed: null,
               ),
             ],
